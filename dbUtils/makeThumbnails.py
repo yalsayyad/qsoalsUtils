e425d61aae3bb68d80343ecb152d39453caf17e8
Yusra AlSayyad <yusra.alsayyad@gmail.com>
2014-08-22 15:20:27 -0500

    add --clobber arg to makeThumbnails.py

--- dbUtils/makeThumbnails.py
+++ dbUtils/makeThumbnails.py
@@ -15,9 +15,12 @@ def makeThumbnails(args):
         head, tail = os.path.split(infile)
         for prefix in ['spec','nspec']:
             filename = tail.replace('spec', prefix).replace('.dir','_overlay.png')
-            os.system('convert %s -thumbnail %s %s'%(os.path.join(infile, filename),
-                                                  args.width,
-                                                  os.path.join(infile, "thumb_" + filename)))
+            thumbnailFilename = os.path.join(infile, "thumb_" + filename)
+            #if it already exists and --clobber =False
+            if (args.clobber or not os.path.isfile(thumbnailFilename)):
+                os.system('convert %s -thumbnail %s %s'%(os.path.join(infile, filename),
+                                                         args.width,
+                                                         os.path.join(infile, "thumb_" + filename)))
 
 
 if __name__ == "__main__":
@@ -26,6 +29,9 @@ if __name__ == "__main__":
                         help = "input directory containing plate subdirectories")
     parser.add_argument("width",
                         help = "width of thumbnails in pixels")
+    parser.add_argument("--clobber",
+                        help = "overwrite existing thumbnails",
+                        action = "store_true")
 
     args = parser.parse_args()
     print args
